1d8dba97033ab046ed878a3176e522d85171ac47
Anton Chudinov <anton.ch89@gmail.com>
2021-07-08 20:08:50 +0500

    Урок 32

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import calcValidation from './modules/calcValidation.js';
 import formValidation from './modules/formValidation.js';
 import calc from './modules/calc.js';
 import sendForm from './modules/sendForm.js';
+import sliderCarusel from './modules/sliderCarusel';
 
 //  Таймер
 countTimer('28 june 2021 10:08:30');
@@ -34,3 +35,24 @@ formValidation();
 calc(100);
 //send ajax-form
 sendForm();
+
+const carusel = new sliderCarusel({
+	main: '.companies-wrapper',
+	wrap: '.companies-hor',
+	slidesToShow: 4,
+	infinity: true,
+	responsive: [{
+		breakpoint: 1024,
+		slidesToShow: 3,
+	},
+	{
+		breakpoint: 768,
+		slidesToShow: 2,
+	},
+	{
+		breakpoint: 576,
+		slidesToShow: 1,
+	}],
+});
+
+carusel.init();
--- src/modules/sliderCarusel.js
+++ src/modules/sliderCarusel.js
@@ -5,25 +5,44 @@ class sliderCarusel {
         wrap,
         next,
         prev,
-        position = 0
+        infinity = false,
+        position = 0,
+        responsive = [],
+        slidesToShow = 4,
     }) {
+        if (!main || !wrap) {
+            console.warn(`slider-carusel: Необходимо 2 свойства, "main" и "wrap"!`);
+        }
         this.main = document.querySelector(main);
         this.wrap = document.querySelector(wrap);
         this.slides = document.querySelector(wrap).children;
         this.next = document.querySelector(next);
         this.prev = document.querySelector(prev);
+        this.slidesToShow = slidesToShow;
         this.options = {
-            position
+            position,
+            infinity,
+            widthSlide: Math.floor(100 / this.slidesToShow),
+            maxPosition: this.slides.length - this.slidesToShow,
         };
+        this.responsive = responsive;
     }
 
     init() {
+
         this.addGloClass();
         this.addStyles();
 
-        if(this.prev && this. next) {
+        if (this.prev && this.next) {
             this.controlSlider();
-        }else {this.addArrow();}
+        } else {
+            this.addArrow();
+            this.controlSlider();
+        }
+        if (this.responsive) {
+            this.responseInit();
+        }
+
     }
     addGloClass() {
         this.main.classList.add('glo-slider');
@@ -33,8 +52,13 @@ class sliderCarusel {
         }
     }
     addStyles() {
-        const style = document.createElement('style');
-        style.id = 'sliderCarusel-style';
+        let style = document.querySelector('#sliderCarusel-style');
+        if(!style){
+            style = document.createElement('style');
+            style.id = 'sliderCarusel-style';
+            document.head.append(style);
+        }
+        
         style.textContent = `
             .glo-slider {
                 overflow: hidden !important;
@@ -45,40 +69,98 @@ class sliderCarusel {
                 will-change: transform !important;
             }
             .glo-slider__item {
-                flex: 0 0 25% !important;
+                display: flex !important;
+                align-items: center;
+                justify-content: center;
+                flex: 0 0 ${this.options.widthSlide}% !important;
                 margin: auto 0 !important;
             }
-            .swiper__next {
+            .glo-slider__next, 
+            .glo-slider__prev {
+                margin: 0 10px;
+                border: 20px solid transparent;
+                background: transparent;
+            }
+            .glo-slider__next {
                 border-left-color: #19b5fe;
             }
-            .swiper__prev {
+            .glo-slider__prev {
                 border-right-color: #19b5fe;
+            }
+            .glo-slider__prev:hover,
+            .glo-slider__next:hover,
+            .glo-slider__prev:focus,
+            .glo-slider__next:focus {
+                background: transparent;
+                outline: transparent;
             }
         }
         `;
-        document.head.append(style);
     }
 
     controlSlider() {
-        this.next.addEventListener('click', this.nextSlider);
-        this.prev.addEventListener('click', this.prevSlider);
+        this.prev.addEventListener('click', this.prevSlider.bind(this));
+        this.next.addEventListener('click', this.nextSlider.bind(this));
     }
 
-    nextSlider(){
-        --this.options.position;
-    }    
+    nextSlider() {
+        if (this.options.infinity || this.options.position < this.options.maxPosition) {
+            ++this.options.position;
+            if (this.options.position > this.options.maxPosition) {
+                this.options.position = 0;
+            }
+            this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`;
+        }
+    }
 
     prevSlider() {
-        ++this.options.position;
+        if (this.options.infinity || this.options.position > 0) {
+            --this.options.position;
+            if (this.options.position < 0) {
+                this.options.position = this.options.maxPosition;
+            }
+            this.wrap.style.transform = `translateX(-${this.options.position * this.options.widthSlide}%)`;
+
+        }
     }
 
     addArrow() {
         this.prev = document.createElement('button');
         this.next = document.createElement('button');
 
-        this.prev.className = 'swiper__prev';
-        this.next.className = 'swiper__next';
+        this.prev.className = 'glo-slider__prev';
+        this.next.className = 'glo-slider__next';
+
         this.main.append(this.prev);
         this.main.append(this.next);
     }
+
+    responseInit() {
+        const slidestToShowDefault = this.slidesToShow,
+            allResponse = this.responsive.map(item => item.breakpoint),
+            maxResponse = Math.max(...allResponse);
+
+            const checkResponse = () => {
+                const widthWindow = document.documentElement.clientWidth;
+    
+                if (widthWindow < maxResponse) {
+                    for (let i = 0; i < allResponse.length; i++) {
+                        if (widthWindow < allResponse[i]) {
+                            this.slidesToShow = this.responsive[i].slidesToShow;
+                            this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+                            this.addStyles();
+                        }
+                    }
+                } else {
+                    this.slidesToShow = slidestToShowDefault;
+                    this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+                    this.addStyles();
+                }
+            };
+    
+            checkResponse();
+            window.addEventListener('resize', checkResponse);
+    }
 }
+
+export default sliderCarusel;
